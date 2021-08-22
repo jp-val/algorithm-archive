4e4cded0049dbf0f79fd5c8618510e592e9a6e04
jp val <valdespino.jp@knights.ucf.edu>
2021-08-21 20:27:23 -0400

    bubble sort

---
+++ BubbleSort.c
@@ -0,0 +1,64 @@
+// JP Valdespino
+// August 2021
+
+// BubbleSort.c
+// ============
+// Worst Case Runtime: O(n^2), where n is the number of elemenst in the array.
+// Best Case Runtime: O(n)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GETLEN(array)  (sizeof(array) / sizeof((array)[0]))
+
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void bubbleSort(int *array, size_t length)
+{
+	int i, swapped;
+	
+	if (array == NULL || length < 1) return;
+
+	swapped = 1;
+
+	while (swapped)
+	{
+		swapped = 0;
+		for (i = 0; i < length-1; i++)
+		{
+			if (array[i] > array[i+1])
+			{
+				swap(&array[i], &array[i+1]);
+				swapped = 1;
+			}
+		}
+	}
+}
+
+void display(int *array, size_t length)
+{
+	int i;
+
+	for (i = 0; i < length; i++)
+		printf("%d%s", array[i], (i < length-1) ? ", " : "\n");
+}
+
+int main(int argc, char **argv)
+{
+	size_t length;
+	int array[] = { 7, -5, 5, 3, -3, 2, 1, 0, -1, -2, 7, -3, -5, -7, 13, -11 };
+	
+	length = GETLEN(array);
+
+	bubbleSort(array, length);
+	display(array, length);
+
+	return 0;
+}
