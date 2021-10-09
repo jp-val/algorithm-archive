1afa809deb177cbeff38160cb9af7c4d2c90695b
jp val <valdespino.jp@knights.ucf.edu>
2021-10-09 00:19:46 -0400

    fixed directed graph cycle detector algorithm

--- ContainsCycle.java
+++ ContainsCycle.java
@@ -5,13 +5,22 @@
 // ==================
 // Different functions for directed and undirected graphs.
 
+// Why different algorithms for unweighted directed and unweighted undirected graphs?
+// ----------------------------------------------------------------------------------
+// Take a two vertex graph for example, vertex A and B in an undirected graph, 
+// an edge connecting these two vertecies does not count as a cycle;
+// But in a directed graph, where there's a directed edge from vertex A to vertex B
+// and there's another directed edge from vertex B to vertex A, then that does count
+// as a cycle, even though these two distinct graphs are practically the same.
+
 import java.io.File;
 import java.io.IOException;
 
-import java.util.Stack;
+import java.util.Queue;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class ContainsCycle
 {
@@ -57,62 +66,76 @@ public class ContainsCycle
 
 	public static boolean containsCycle_forDirectedGraphs(int[][] g)
 	{
-		boolean[] visited = new boolean[g.length];
-		Stack<Integer> stack = new Stack<>();
+		int[] incoming = new int[g.length];
 
-		stack.push(0);
+		for (int i = 0; i < g.length; i++)
+			for (int j = 0; j < g[i].length; j++)
+				if (g[i][j] < Graph.oo)
+					incoming[j]++;
+		
+		Queue<Integer> q = new LinkedList<Integer>();
 
-		while (!stack.isEmpty())
+		for (int i = 0; i < incoming.length; i++)
+			if (incoming[i] <= 0)
+				q.add(i);
+
+		int unprocessed = g.length;
+		
+		while (!q.isEmpty())
 		{
-			Integer vertex = stack.pop();
+			Integer v = q.remove();
+			unprocessed--;
 
-			if (visited[vertex]) return true;
-
-			visited[vertex] = true;
-
-			for (int i = 0; i < g.length; i++)
-				if (g[vertex][i] < Graph.oo)
-					stack.add(i);
-
-			visited[vertex] = false;
+			for (int i = 0; i < g[v].length; i++)
+				if (g[v][i] < Graph.oo)
+					if (--incoming[i] <= 0)
+						q.add(i);
 		}
 
-		return false;
+		return unprocessed > 0;
 	}
 
 	public static boolean containsCycle_forDirectedGraphs(ArrayList<ArrayList<Edge>> g)
 	{
-		boolean[] visited = new boolean[g.size()];
-		Stack<Integer> stack = new Stack<>();
+		int[] incoming = new int[g.size()];
 
-		stack.push(0);
-
-		while (!stack.isEmpty())
-		{
-			Integer vertex = stack.pop();
-
-			if (visited[vertex]) return true;
-
-			visited[vertex] = true;
-
-			for (Edge e: g.get(vertex))
-				stack.push(e.vertex);
-			
-			visited[vertex] = false;
-		}
+		for (int i = 0; i < g.size(); i++)
+			for (Edge e: g.get(i))
+				incoming[e.vertex]++;
 		
-		return false;
+		Queue<Integer> q = new LinkedList<Integer>();
+
+		for (int i = 0; i < incoming.length; i++)
+			if (incoming[i] <= 0)
+				q.add(i);
+
+		int unprocessed = g.size();
+
+		while (!q.isEmpty())
+		{
+			Integer v = q.remove();
+			unprocessed--;
+
+			for (Edge e: g.get(v))
+				if (--incoming[e.vertex] <= 0)
+					q.add(e.vertex);
+		}
+
+		return unprocessed > 0;
 	}
 
 	public static void main(String[] args) throws IOException
 	{
-		Graph g1 = new Graph("graph-undirected-petersen.in");
-		Graph g2 = new Graph("graph-undirected-tree.in");
+		Graph g1 = new Graph("graphs/graph-undirected-petersen.in");
+		Graph g2 = new Graph("graphs/graph-undirected-tree.in");
+		Graph g3 = new Graph("graphs/graph-directed-scc.in");
 
 		System.out.println("contains cycle petersen graph (adjMatrix): " + containsCycle_forUndirectedGraphs(g1.adjMatrix));
 		System.out.println("contains cycle petersen graph (adjList): " + containsCycle_forUndirectedGraphs(g1.adjList));
 		System.out.println("contains cycle tree graph (adjMatrix): " + containsCycle_forUndirectedGraphs(g2.adjMatrix));
 		System.out.println("contains cycle tree graph (adjList): " + containsCycle_forUndirectedGraphs(g2.adjList));
+		System.out.println("contains cycle directed scc (adjMatrix): " + containsCycle_forDirectedGraphs(g3.adjMatrix));
+		System.out.println("contains cycle directed scc graph (adjList): " + containsCycle_forDirectedGraphs(g3.adjList));
 	}
 }
 
